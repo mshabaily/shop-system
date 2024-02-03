af782a3952fa8bbf77a6d4941bf6e13ecd2f1b10
Matthew Shabaily <matthew.shabaily@icloud.com>
2024-02-03 03:35:19 +0000

    added headers, revamped password change

--- table.py
+++ table.py
@@ -1,32 +1,53 @@
-from tkinter import END, X, Entry, Frame, Menu, messagebox
+from tkinter import END, X, Entry, Frame, Label, Menu, messagebox
 from settings import *
 from gui import window
 
-class Table:
-    def __init__(self,objects,canvas):
-        attributes = list(vars(objects[0]).keys())
-        tableContainer = Frame(canvas, background= tableHighlight)
-        canvas.create_window((0,0), width = canvas.winfo_reqwidth(), window = tableContainer, anchor = "nw")
-        canvas.configure(background = canvasBackground)
-        canvas.configure(highlightthickness = canvasBorderWidth, highlightbackground = canvasBorderColour)
-        for row, item in enumerate(objects):
-            rowContainer = Row(tableContainer)
-            for column, attribute in enumerate(attributes):
+class Table(Frame):
+    def __init__(self,objects,canvas,accessLevel):
+        super().__init__()
+        self.attributes = list(vars(objects[0]).keys())
+        self.tableContainer = Frame(canvas, background= tableHighlight)
+        self.canvas = canvas
+        self.objects = objects
+        self.accessLevel = accessLevel
+    def load(self):
+        self.canvas.create_window((0,0), width = self.canvas.winfo_reqwidth(), window = self.tableContainer, anchor = "nw")
+        self.canvas.configure(background = canvasBackground)
+        self.canvas.configure(highlightthickness = canvasBorderWidth, highlightbackground = canvasBorderColour)
+        for row, item in enumerate(self.objects, 0):
+            rowContainer = Row(self.tableContainer, self)
+            for column, attribute in enumerate(self.attributes):
                 rowContainer.grid_columnconfigure(index=column, weight = 1)
-                field = Item(rowContainer, item, attribute)
-                field.grid(row = row, column = column)
+                if row == 0:
+                    header = Label(master = rowContainer, text = attribute, font = bodyFont, bg = appColour)
+                    header.grid(row = 0, column = column)
+                field = Item(rowContainer, item, attribute, self.accessLevel)
+                field.grid(row = row+1, column = column)
             rowContainer.pack(fill=X, anchor = "nw", pady = 1)
-        scrollableDepth = ((len(objects)+1) * field.winfo_reqheight())
-        canvas.config(scrollregion = (0,0,0,scrollableDepth))
-
+        scrollableDepth = ((len(self.objects)+1) * field.winfo_reqheight())
+        self.canvas.config(scrollregion = (0,0,0,scrollableDepth))
+        self.menu = Menu(tearoff=False)
+        self.menu.add_command(label = "Add Row", command = self.add)
+        self.bind("<Button-3>", lambda event: self.popup())
+    def popup(self):
+        root = self.winfo_toplevel()
+        self.menu.tk_popup(root.winfo_pointerx(), root.winfo_pointery())
+    def clear(self):
+        for child in self.winfo_children():
+            child.destroy()
+    def add(self):
+        self.objects.append(self.objects[0])
+        self.clear()
+        self.load()
+        
 class Row(Frame):
-    def __init__(self, master, **kwargs):
+    def __init__(self, master, table, **kwargs):
         super().__init__(master, background= rowBorderColour, border = rowBorderWidth, **kwargs)
         self.menu = Menu(tearoff=False)
-        self.menu.add_command(label = "Delete", command = self.delete)
+        self.menu.add_command(label = "Add Row", command = table.add)
+        self.menu.add_command(label = "Delete Row", command = self.delete)
         self.bind("<Button-3>", lambda event: self.popup())
     def delete(self):
-        self.winfo_children()[0].item.forget()
         self.destroy()
     def popup(self):
         root = self.winfo_toplevel()
@@ -34,22 +55,22 @@ class Row(Frame):
     def bind_child(self,child):
         child.bind("<Button-3>", lambda event: self.popup())
 
-def infoEntryHandler(event,object,attribute,field):
-    global user
+def infoEntryHandler(event,object,attribute):
     if event.keysym == "Return" or event.keysym == "Escape":
         return
-    if window.authority != "staff":
+    if window.user.status != "staff" or event.widget.accessLevel == "open":
         object.__setattr__(attribute,event.widget.get())
     else:
         messagebox.showerror("You do not have permission to change this")
-        field.delete(0,END)
-        field.insert(0,getattr(object,attribute))
+        event.widget.delete(0,END)
+        event.widget.insert(0,getattr(object,attribute))
         
 class Item(Entry):
-    def __init__(self, master, item, attribute, **kwargs):
+    def __init__(self, master, item, attribute, accessLevel, **kwargs):
         super().__init__(master, font = tableFont, background= rowBackground, **kwargs)
+        self.accessLevel = accessLevel
         self.item = item
         self.insert(0,getattr(item, attribute))
-        response = lambda event, item = item, attribute = attribute, self = self: infoEntryHandler(event,item,attribute,self)
+        response = lambda event, item = item, attribute = attribute, self = self: infoEntryHandler(event,item,attribute)
         self.bind("<KeyRelease>", response)
         master.bind_child(self)
